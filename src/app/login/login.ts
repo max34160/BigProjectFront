d8a6066b55e57b9aa5d9d525d1718331a1d254ad
max34 <maximegaussen34160@gmail.com>
2026-04-07 15:52:26 +0200

    V1

---
+++ src/app/login/login.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class Login {
+  nom = "";
+  prenom = "";
+  age = "";
+  password = "";
+
+}
