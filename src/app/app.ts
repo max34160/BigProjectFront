250a1f3659f10a0341ed6795da8c8eb437e3f5dd
max34 <maximegaussen34160@gmail.com>
2026-04-08 11:14:17 +0200

    v3 layout fait

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {MatTabsModule} from '@angular/material/tabs';
+import { RouterOutlet, RouterLinkWithHref, RouterLink } from '@angular/router';
+import {MatTabsModule, MatTab, MatTabGroup, MatTabLink } from '@angular/material/tabs';
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,MatTabsModule],
+  imports: [RouterOutlet, MatTabsModule, MatTab, MatTabGroup, MatIcon, RouterLinkWithHref, RouterLink, MatTabLink],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
