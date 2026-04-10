259c632a3cd30ee1b674607172939faa51f86158
CamossiJeremy <camossijeremy@gmail.com>
2026-04-10 15:50:11 +0200

    resolution conflits

--- src/app/lib/api.ts
+++ src/app/lib/api.ts
@@ -1,46 +1,38 @@
-<<<<<<< HEAD
-  export class ApiService {
-    form: any;
-apiRoot = 'http://localhost:5500/api';
-    async register(): Promise<void> {
-      const reponse = await fetch(`${this.apiRoot}/user/`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          nom: this.form.value.nom,
-          prenom: this.form.value.prenom,
-          age: this.form.value.age,
-          email: this.form.value.email,
-          motDePasse: this.form.value.motDePasse
-        })
-      });
-      const data = await reponse.json();
-      console.log('Utilisateur enregistré:',data);
-    }
-  }
-=======
-
-
+import { FormGroup } from "@angular/forms";
 
 const apiRoot = "http://localhost:5500/api";
 
 export async function login(email: string, password: string) {
 
-    const reponse = await fetch(apiRoot + "session", {
-        method: "GET",
-        headers: {
-            'Content-Type': "application/json"
-        },
-        body: JSON.stringify({
-            email: email,
-            password: password
-        })
-    });
-    const data = await reponse.json();
+  const reponse = await fetch(apiRoot + "session", {
+    method: "GET",
+    headers: {
+      'Content-Type': "application/json"
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password
+    })
+  });
+  const data = await reponse.json();
 
-    return data ;
-
+  return data;
 }
->>>>>>> 0276949736f9a9adc601b0dffd668883ac98d965
+
+export async function register(form: FormGroup): Promise<void> {
+  const reponse = await fetch(`${apiRoot}/user/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      nom: form.value.nom,
+      prenom: form.value.prenom,
+      age: form.value.age,
+      email: form.value.email,
+      motDePasse: form.value.motDePasse
+    })
+  });
+  const data = await reponse.json();
+  console.log('Utilisateur enregistré:', data);
+}
