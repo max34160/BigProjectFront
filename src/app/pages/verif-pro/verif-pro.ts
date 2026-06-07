ddc839c39e573d2f5e69d7dd5ac7dde16fe87472
CamossiJeremy <camossijeremy@gmail.com>
2026-06-07 15:28:45 +0200

    ajout du verif pro

--- src/app/pages/verif-pro/verif-pro.ts
+++ src/app/pages/verif-pro/verif-pro.ts
@@ -1,21 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatFormField, MatLabel } from "@angular/material/form-field";
 import { MatInput } from "@angular/material/input";
-import { FormBuilder, FormGroup, ReactiveFormsModule , Validators } from "@angular/forms";
-import { RouterLink } from "@angular/router";
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { RouterLink, Router } from "@angular/router";
+import { MatButton } from "@angular/material/button";
+import { GlobalService } from "../../services/global";
+import * as API from "../../lib/api";
 
 @Component({
   selector: 'app-verif-pro',
-  imports: [MatFormField, MatLabel, MatInput, ReactiveFormsModule, RouterLink],
+  imports: [MatFormField, MatLabel, MatInput, ReactiveFormsModule, RouterLink, MatButton],
   templateUrl: './verif-pro.html',
   styleUrl: './verif-pro.scss',
 })
 export class VerifPro {
   form: FormGroup;
+  cabinetForm: FormGroup;
+  medecinInfo: any = null;
+  errorMessage = '';
+  private router = inject(Router);
 
-  constructor(private fb: FormBuilder){
+  constructor(private fb: FormBuilder, public global: GlobalService) {
     this.form = this.fb.group({
       idNat: ['', [Validators.required]],
     });
+    this.cabinetForm = this.fb.group({
+      nom_cabinet: ['', [Validators.required]],
+      description: [''],
+      horaire_cabinet: [''],
+    });
+  }
+
+  async verify() {
+    this.errorMessage = '';
+    const result = await API.verifyPro(this.form.value.idNat);
+    if (result.error) {
+      this.errorMessage = result.error;
+    } else {
+      this.medecinInfo = result;
+    }
+  }
+
+  async register() {
+    await API.registerPro(
+      this.form.value.idNat,
+      this.global.user.id_user,
+      this.cabinetForm.value.nom_cabinet,
+      this.cabinetForm.value.description,
+      this.cabinetForm.value.horaire_cabinet,
+    );
+    this.router.navigate(['/profil']);
   }
 }
