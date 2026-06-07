e4377dc2949dec65ebb2c0e05478ced1e561cf36
CamossiJeremy <camossijeremy@gmail.com>
2026-06-07 15:12:58 +0200

    commit

--- src/app/lib/api.ts
+++ src/app/lib/api.ts
@@ -1,5 +1,3 @@
-import { FormGroup } from "@angular/forms";
-
 const apiRoot = "http://localhost:5500/api";
 
 export async function login(email: string, password: string) {
@@ -37,7 +35,7 @@ export async function register(nom: string, prenom: string , age : string , emai
     const data = await reponse.json();
     console.log('Utilisateur enregistré:', data);
     return data;
-    
+
 }
 
 export async function getAllMethodologie() {
@@ -93,6 +91,69 @@ export async function isPro(id_user: number) {
         headers: {
             'Content-Type': "application/json"
         }
+    });
+    const data = await reponse.json();
+
+    return data;
+
+}
+
+export async function getUser(id: string) {
+
+    const reponse = await fetch(apiRoot + "/user/" + id, {
+        method: "GET",
+        credentials: "include",
+        headers: {
+            'Content-Type': "application/json"
+        }
+    });
+    const data = await reponse.json();
+
+    return data;
+
+}
+
+export async function updateUser(id: string, nom: string, prenom: string, age: string, email: string) {
+
+    const reponse = await fetch(apiRoot + "/user/" + id, {
+        method: "PUT",
+        credentials: "include",
+        headers: {
+            'Content-Type': "application/json"
+        },
+        body: JSON.stringify({ nom, prenom, age, email })
+    });
+    const data = await reponse.json();
+
+    return data;
+
+}
+
+export async function verifyPro(identificationNationale: string) {
+
+    const reponse = await fetch(apiRoot + "/pro/verify", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+            'Content-Type': "application/json"
+        },
+        body: JSON.stringify({ identificationNationale })
+    });
+    const data = await reponse.json();
+
+    return data;
+
+}
+
+export async function registerPro(identificationNationale: string, id_user: number, nom_cabinet: string, description: string, horaire_cabinet: string) {
+
+    const reponse = await fetch(apiRoot + "/pro/register", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+            'Content-Type': "application/json"
+        },
+        body: JSON.stringify({ identificationNationale, id_user, nom_cabinet, description, horaire_cabinet })
     });
     const data = await reponse.json();
 
